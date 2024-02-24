dcda78b40f27da5de37dfd1b1ffaf1845b16b6ba
unknown <prince.yadavp098@gmail.com>
2024-02-24 16:49:39 +0530

    initial commit

---
+++ frontend/index.js
@@ -0,0 +1,48 @@
+(function () {
+    const API = "http://api.weatherapi.com/v1";
+  
+    new Vue({
+      el: "#app",
+      data: {
+        showModal: false,
+        search:'Mumbai',
+        weatherObj: {},
+        toast: {
+          type: "danger",
+          message: null,
+          show: false
+        }
+      },
+      mounted() {
+        this.getWeatherData();
+      },
+      methods: {
+        getWeatherData() {
+           axios
+            .get(`${API}/current.json?key=9065ab7a89f4407d8df91550220807&q=${this.search}`)
+            .then((response) => {
+                debugger;
+              this.weatherObj = response.data;
+              this.search = '';
+            })
+            .catch((err) => {
+              this.showError("Get", err.message);
+            });
+        },
+        showError(action, message) {
+          this.showToast(`${action} failed: ${message}`, "danger");
+        },
+        showSuccess(message) {
+          this.showToast(message, "success");
+        },
+        showToast(message, type) {
+          this.toast.message = message;
+          this.toast.show = true;
+          this.toast.type = type;
+          setTimeout(() => {
+            this.toast.show = false;
+          }, 3000);
+        }
+      }
+    });
+  })();
